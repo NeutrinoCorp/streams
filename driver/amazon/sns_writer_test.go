ae38b62398493d3ef1c446ed711acc8fbf8e7035
aruizeac <luis.alonso.16@hotmail.com>
2022-04-10 15:06:28 -0600

    Add initial Amazon SNS and SQS writer implementations

---
+++ driver/amazon/sns_writer_test.go
@@ -0,0 +1,148 @@
+//go:build integration
+
+package amazon_test
+
+import (
+	"context"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+	"github.com/google/uuid"
+	"github.com/neutrinocorp/streamhub"
+	"github.com/neutrinocorp/streamhub/driver/amazon"
+	"github.com/stretchr/testify/suite"
+)
+
+var snsTopics = []string{
+	"ncorp-dev-sample_service-action"}
+
+type SnsWriterSuite struct {
+	suite.Suite
+	accountID string
+	cfgAws    aws.Config
+	client    *sns.Client
+	writer    amazon.SnsWriter
+	marshaler streamhub.Marshaler
+
+	topicBuffer sync.Map
+}
+
+func TestNewSnsWriter(t *testing.T) {
+	suite.Run(t, &SnsWriterSuite{})
+}
+
+func (s *SnsWriterSuite) SetupSuite() {
+	s.accountID = defaultLocalAwsAccountID
+	s.cfgAws = defaultLocalAwsConfig
+	s.client = sns.NewFromConfig(s.cfgAws)
+	s.topicBuffer = sync.Map{}
+	s.createSnsTopics()
+	s.writer = amazon.NewSnsWriter(s.client, s.accountID, s.cfgAws.Region)
+	s.marshaler = streamhub.JSONMarshaler{}
+}
+
+func (s *SnsWriterSuite) createSnsTopics() {
+	var err error
+	wg := sync.WaitGroup{}
+	wg.Add(len(snsTopics))
+	for i := 0; i < len(snsTopics); i++ {
+		go func(index int) {
+			defer wg.Done()
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*8)
+			defer cancel()
+			out, errScoped := s.client.CreateTopic(ctx, &sns.CreateTopicInput{
+				Name:       aws.String(snsTopics[index]),
+				Attributes: nil,
+				Tags:       nil,
+			})
+			if errScoped != nil && !strings.Contains(errScoped.Error(), "TopicAlreadyExists") {
+				err = errScoped
+				return
+			}
+			s.topicBuffer.Store(snsTopics[index], out.TopicArn)
+		}(i)
+	}
+	wg.Wait()
+	s.Require().NoError(err)
+}
+
+func (s *SnsWriterSuite) TearDownSuite() {
+	s.removeSnsTopics()
+}
+
+func (s *SnsWriterSuite) removeSnsTopics() {
+	var err error
+	wg := sync.WaitGroup{}
+	wg.Add(len(snsTopics))
+	for i := 0; i < len(snsTopics); i++ {
+		go func(index int) {
+			defer wg.Done()
+			rawArn, ok := s.topicBuffer.Load(snsTopics[index])
+			if !ok {
+				return
+			}
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*8)
+			defer cancel()
+			_, errScoped := s.client.DeleteTopic(ctx, &sns.DeleteTopicInput{
+				TopicArn: rawArn.(*string),
+			})
+			if errScoped != nil {
+				err = errScoped
+			}
+		}(i)
+	}
+	wg.Wait()
+	s.Require().NoError(err)
+}
+
+func (s *SnsWriterSuite) TestSnsWriter_Write() {
+	eventRaw, err := s.marshaler.Marshal("", sampleEventFoo{
+		Foo: "lorem ipsum",
+		Bar: "dolor sit amet",
+	})
+	s.Require().NoError(err)
+	err = s.writer.Write(context.Background(), streamhub.NewMessage(streamhub.NewMessageArgs{
+		SchemaVersion:        1,
+		Data:                 eventRaw,
+		ID:                   uuid.NewString(),
+		Source:               "",
+		Stream:               snsTopics[0],
+		SchemaDefinitionName: "",
+		ContentType:          s.marshaler.ContentType(),
+		Subject:              "",
+	}))
+	s.Assert().NoError(err)
+}
+
+func (s *SnsWriterSuite) TestSnsWriter_WriteBatch() {
+	eventRaw, err := s.marshaler.Marshal("", sampleEventFoo{
+		Foo: "lorem ipsum",
+		Bar: "dolor sit amet",
+	})
+	s.Require().NoError(err)
+	out, err := s.writer.WriteBatch(context.Background(), streamhub.NewMessage(streamhub.NewMessageArgs{
+		SchemaVersion:        1,
+		Data:                 eventRaw,
+		ID:                   uuid.NewString(),
+		Source:               "",
+		Stream:               snsTopics[0],
+		SchemaDefinitionName: "",
+		ContentType:          s.marshaler.ContentType(),
+		Subject:              "",
+	}), streamhub.NewMessage(streamhub.NewMessageArgs{
+		SchemaVersion:        1,
+		Data:                 eventRaw,
+		ID:                   uuid.NewString(),
+		Source:               "",
+		Stream:               snsTopics[0],
+		SchemaDefinitionName: "",
+		ContentType:          s.marshaler.ContentType(),
+		Subject:              "",
+	}))
+	s.Assert().NoError(err)
+	s.Assert().Equal(uint32(2), out)
+}
